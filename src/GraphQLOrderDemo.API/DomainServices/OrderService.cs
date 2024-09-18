8260dbe4e3024cd5dd1f98a5fa4221f7c8058027
Skuirrels <ty.omidi@outlook.com>
2024-09-18 16:59:00 +0100

    Updated Nuget Packages

---
+++ src/GraphQLOrderDemo.API/DomainServices/OrderService.cs
@@ -0,0 +1,26 @@
+using GraphQLOrderExample.Data;
+using GraphQLOrderExample.DomainModels;
+using HotChocolate.Pagination;
+using Microsoft.EntityFrameworkCore;
+// using PagingQueryableExtensions = HotChocolate.Data.PagingQueryableExtensions;
+
+namespace GraphQLOrderExample.Services;
+
+public sealed class OrderService(OrderContext context)
+{
+    public async Task<Order?> GetOrderByIdAsync(
+        Guid id, 
+        CancellationToken cancellationToken)
+    {
+        return await context.Orders.FirstOrDefaultAsync(i => i.Id == id, cancellationToken);
+    }
+    
+    public async Task<Page<Order>> GetOrdersAsync(
+        PagingArguments pagingArguments,
+        CancellationToken cancellationToken = default)
+        => await PagingQueryableExtensions.ToPageAsync(context.Orders
+                .AsNoTracking()
+                .OrderBy(t => t.OrderNumber)
+                .ThenBy(i => i.Id), pagingArguments, cancellationToken);
+}
+
